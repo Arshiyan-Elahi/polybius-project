d8643b5e6e38be31988416905760389bf5ced628
Arshiyan Elahi <144221602+Arshiyan-Elahi@users.noreply.github.com>
2025-06-26 17:38:15 +0500

    Add files via upload

--- api_client.py
+++ api_client.py
@@ -168,4 +168,5 @@ def index():
     return render_template_string(html_template, encrypted=encrypted, matrix=matrix)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    url = 'https://polybius-project.onrender.com'
+
